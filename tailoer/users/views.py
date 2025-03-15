3aabff1fd9ae28c37987ae8c1b73e18723aa8c63
Wagner Wieland <wagnerp5@hotmail.com>
2025-03-15 19:25:59 +0000

    authorization and login

--- tailoer/users/views.py
+++ tailoer/users/views.py
@@ -1,9 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 from .models import Profile
+
+
+
+def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect('profile', request.user.username)
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print("Username does not exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profile', user)
+        else:
+            print("Username OR password is incorrect")
+
+    return render(request, "users/login.html")
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 
 
 def profile(request, username):
     profile = Profile.objects.get(username=username)
-    context = {'profile': profile}
+    products = profile.product_set.all()
+    context = {'profile': profile, 'products': products}
     return render(request, 'users/profile.html', context)
